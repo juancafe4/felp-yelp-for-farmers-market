84c9579b883caab9d5a83a3225d763998aac29d5
Zhaggym <zhaggm@gmail.com>
2016-08-28 11:44:07 -0700

    Added Review Model

---
+++ server/models/Review.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const ReviewSchema = new Schema({
+  rating: {type: Number, max: 5, min: 1},
+  text: {type: String, required: true},
+  date: {type: Date, default: Date.now}
+})
+const Review = mongoose.model('Review', ReviewSchema);
+
+module.exports = Review;
